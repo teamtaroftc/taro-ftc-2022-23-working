66fc5530eee40331b01c5ed560512b1547f023a1
ssmoogi <ssmoogi@gmail.com>
2023-01-08 01:46:55 -0800

    add files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Constants.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+public class Constants {
+
+    // Linear Slides
+
+    public static int high = 3000;
+    public static int mid = 2000;
+    public static int low = 1200;
+    public static int junction = 250;
+    public static int base = 0;
+
+    // Claw
+
+    public static double CLOSE_POS = 0.5; // close pos
+    public static double OPEN_POS = 0; // open pos
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Drive.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Drive {
+
+    public Drive (HardwareMap hardwareMap) {
+
+    }
+
+}
